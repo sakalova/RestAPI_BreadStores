ac6677c58d1a592ebbd036f8807c8b6e55723470
maria <sakalovami@gmail.com>
2023-05-13 01:16:36 +0100

    feat(DB models): initialized database models

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .bread_model import BreadModel
+from .bakery_model import BakeryModel
+from .tag_model import TagModel
+from .bread_tags_model import BreadsTagsModel
+from .user_model import UserModel
