93e39641700cd77e4c44712c072c2957328a5568
maria <sakalovami@gmail.com>
2023-05-13 01:12:56 +0100

    refactor(emails): url build

--- emails.py
+++ emails.py
@@ -2,10 +2,13 @@ import os
 import requests
 
 
+MAILGUN_BASE_URL = "https://api.mailgun.net/v3"
+
+
 def send_message(to: str, subject: str, text: str) -> requests.Response:
 	api_key = os.getenv("MAILGUN_API_KEY")
 	domain_name = os.getenv("MAILGUN_DOMAIN_NAME")
-	url = f"https://api.mailgun.net/v3/{domain_name}/messages"
+	url = f"{MAILGUN_BASE_URL}/{domain_name}/messages"
 	data = {
 		"from": f"Maria <postmaster@{domain_name}>",
 		"to": [to],
