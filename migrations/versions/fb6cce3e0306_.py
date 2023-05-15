1d7dc08fdc0fe8838faa303ddba6482c486a300b
maria <sakalovami@gmail.com>
2023-05-15 12:02:28 +0100

    added mugrations

---
+++ migrations/versions/fb6cce3e0306_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fb6cce3e0306
+Revises: 
+Create Date: 2023-05-15 11:38:17.078196
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fb6cce3e0306'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
