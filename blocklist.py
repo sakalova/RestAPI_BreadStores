cf06e3cb12e21d01bebbd0da7e9b7e8c7f3800db
maria <sakalovami@gmail.com>
2023-05-17 14:00:13 +0100

    itinialized mypy and added types-requests lib to requirements.txt

--- blocklist.py
+++ blocklist.py
@@ -3,5 +3,6 @@ This file just contains the blocklist of the JWT tokens. It will be imported by
 app and the logout resource so that tokens can be added to the blocklist when the
 user logs out.
 """
+from typing import Set
 
-BLOCKLIST = set()
+BLOCKLIST: Set[str] = set()
